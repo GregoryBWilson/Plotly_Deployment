0c3260feeca5d00d67924e9f78b99aeb92fb1832
Greg Wilson <greg@gbwilson.com>
2022-01-30 17:16:17 -0500

    Was working on 127.0.0.1:8000 will it work on GitHub Page?

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,37 +1,48 @@
-// Sort the data array using the greekSearchResults value
-data.sort(function(a, b) {
-  return parseFloat(b.greekSearchResults) - parseFloat(a.greekSearchResults);
-});
+function optionChanged(newSample) {
+  buildMetadata(newSample);
+  buildCharts(newSample);
+}
 
-// Slice the first 10 objects for plotting
-data = data.slice(0, 10);
+function buildMetadata(sample) {
+  d3.json("samples.json").then((data) => {
+    var metadata = data.metadata;
+    var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+    var result = resultArray[0];
+    var PANEL = d3.select("#sample-metadata");
 
-// Reverse the array due to Plotly's defaults
-data = data.reverse();
+    PANEL.html("");
+    ageText = "age:".toUpperCase().concat(result.age)
+    bbtypeText = "bbtype:".toUpperCase().concat(result.bbtype)
+    ethnicityText = "ethnicity:".toUpperCase().concat(result.ethnicity)
+    genderText = "gender:".toUpperCase().concat(result.gender)
+    idText = "id:".toUpperCase().concat(result.id)
+    locationText = "location:".toUpperCase().concat(result.location)
+    wfreqText = "wfreq:".toUpperCase().concat(result.wfreq)
+        
+    PANEL.append("h6").text(idText);
+    PANEL.append("h6").text(ethnicityText);
+    PANEL.append("h6").text(genderText);
+    PANEL.append("h6").text(ageText);
+    PANEL.append("h6").text(locationText);
+    PANEL.append("h6").text(bbtypeText);
+    PANEL.append("h6").text(wfreqText);
+    
+  });
+}
 
-// Trace1 for the Greek Data
-var trace1 = {
-  x: data.map(row => row.greekSearchResults),
-  y: data.map(row => row.greekName),
-  text: data.map(row => row.greekName),
-  name: "Greek",
-  type: "bar",
-  orientation: "h"
-};
-
-// data
-var data = [trace1];
-
-// Apply the group bar mode to the layout
-var layout = {
-  title: "Greek gods search results",
-  margin: {
-    l: 100,
-    r: 100,
-    t: 100,
-    b: 100
-  }
-};
-
-// Render the plot to the div tag with id "plot"
-Plotly.newPlot("plot", data, layout);
+function init() {
+    var selector = d3.select("#selDataset");
+  
+    d3.json("samples.json").then((data) => {
+      console.log(data);
+      var sampleNames = data.names;
+      sampleNames.forEach((sample) => {
+        selector
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+  })}
+  
+  init();
+  
